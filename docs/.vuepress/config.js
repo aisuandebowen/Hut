f1a1365d81c944bf24cea1bd451468849840eb5a
Boen <1836140285@qq.com>
2023-09-04 20:36:20 +0800

    perf: 文档分类

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@
  * @Author: cbw
  * @Date: 2023-08-10 21:25:06
  * @LastEditors: cbw
- * @LastEditTime: 2023-09-03 18:54:06
+ * @LastEditTime: 2023-09-04 20:32:54
  * @Description:
  */
 module.exports = {
@@ -18,16 +18,21 @@ module.exports = {
       lang: "zh-CN",
     },
   },
-  plugins: [["vuepress-plugin-code-copy",{
-    color:'#c9c2c2'
-  }]],
+  plugins: [
+    [
+      "vuepress-plugin-code-copy",
+      {
+        color: "#c9c2c2",
+      },
+    ],
+  ],
   theme: "reco",
   themeConfig: {
     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
     repo: "https://github.com/aisuandebowen/Hut",
     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
     // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    repoLabel: "查看源码",
+    repoLabel: "Github",
     // 假如文档不是放在仓库的根目录下：
     docsDir: "docs",
     // 假如文档放在一个特定的分支下：
@@ -39,8 +44,17 @@ module.exports = {
     subSidebar: "auto",
     nav: [
       { text: "首页", link: "/" },
-      { text: "简法主页", link: "https://www.jianfast.com/" },
-      // { text: "GitHub", link: "https://github.com/aisuandebowen" },
+      {
+        text: '常用网站',
+        ariaLabel: 'Common Website',
+        items: [
+          { text: "GitHub", link: "https://github.com" },
+          { text: "翻译", link: "https://fanyi.youdao.com/indexLLM.html#/" },
+          { text: "MDN", link: "https://developer.mozilla.org/en-US/" },
+          { text: "现代JavaScript教程", link: "https://zh.javascript.info/" },
+          { text: "简法主页", link: "https://www.jianfast.com/" },
+        ]
+      }
       // {
       //   text: "CSDN",
       //   link: "https://blog.csdn.net/weixin_44173943?spm=1000.2115.3001.5343",
@@ -48,7 +62,7 @@ module.exports = {
     ],
     sidebar: [
       {
-        title: "简介",
+        title: "导语",
         path: "/",
       },
       {
@@ -56,14 +70,28 @@ module.exports = {
         path: "/code-snippet/",
       },
       {
-        title: "学习笔记",
-        path: "/note/",
+        title: "前端",
         collapsable: false,
         children: [
-          { title: "Git", path: "/note/git/" },
-          { title: "Note", path: "/note/study/" },
+          { title: "JavaScript", path: "/note/JavaScript/JavaScript.md" },
+          { title: "TS", path: "/note/TS/TS.md" },
+          { title: "Vue2", path: "/note/Vue2/Vue2.md" },
+          { title: "Vue3", path: "/note/Vue3/Vue3.md" },
+          { title: "HTML CSS", path: "/note/HTML-CSS/HTML-CSS.md" },
         ],
       },
+      {
+        title: "Git",
+        path: "/note/git/git.md",
+      },
+      {
+        title:"需求开发思考",
+        path: "/note/ponder/ponder.md",
+      },
+      {
+        title:"其他",
+        path: "/note/other/other.md",
+      }
     ],
   },
 };
--- docs/README.md
+++ docs/README.md
@@ -2,10 +2,10 @@
  * @Author: cbw
  * @Date: 2023-08-10 21:14:24
  * @LastEditors: cbw
- * @LastEditTime: 2023-09-03 20:25:13
+ * @LastEditTime: 2023-09-04 20:27:46
  * @Description:
 -->
 
-# 简介
+# 导语
 
 向花布示爱，向苹果致敬。
