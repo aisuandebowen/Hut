f50cfc641cad48dd51590ec5d7e91db5ab49a077
Boen Chen <58322181+aisuandebowen@users.noreply.github.com>
2023-10-23 17:19:59 +0800

    Create index.js

---
+++ temp/vue2-easy-print/index.js
@@ -0,0 +1,32 @@
+import Iframe from "./src/Iframe";
+import PrintFrame from "./src/print-frame.vue";
+import Vue from "vue";
+import { copyDom } from "./src/utils";
+
+/**
+ * 打印
+ * @param {String} id
+ */
+export function print(options) {
+  const { id } = options;
+  try {
+    const Frame = Vue.extend(PrintFrame);
+    const content = document.getElementById(id);
+    const vm = new Frame({
+      propsData: {
+        ...options,
+        renderContent() {
+          return copyDom(content);
+        },
+      },
+    });
+    vm.$mount();
+
+    // 初始化iframe
+    const iframe = new Iframe();
+    document.body.appendChild(iframe.dom);
+    iframe.print(id, vm.$el); // 打印
+  } catch (error) {
+    console.error(error);
+  }
+}
